1f0b3708436b996b22ea0f50dd307ff23a2f4f60
Piotr Grzegorzynski <pgrzegorzynski@gmail.com>
2020-07-27 21:01:56 +0200

    Updates

---
+++ precache-manifest.91b52489392e9a66e47c02def9bcba6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb453a19f3d68b6d821ded690f031df",
+    "url": "/to-do-list-with-react.js/index.html"
+  },
+  {
+    "revision": "67e792f3d1741002421c",
+    "url": "/to-do-list-with-react.js/static/css/main.d08a6f47.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/to-do-list-with-react.js/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-with-react.js/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e792f3d1741002421c",
+    "url": "/to-do-list-with-react.js/static/js/main.05e6bc56.chunk.js"
+  },
+  {
+    "revision": "4c9ded894363221da8b6",
+    "url": "/to-do-list-with-react.js/static/js/runtime-main.dc51ddeb.js"
+  }
+]);
